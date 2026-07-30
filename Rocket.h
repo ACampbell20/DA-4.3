23ca713829337cf5b6ee7fcb66b85a3bcc474b0e
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 10:16:53 -0500

    Add Rocket class definition with move method

---
+++ Rocket.h
@@ -0,0 +1,15 @@
+#ifndef ROCKET_H
+#define ROCKET_H
+
+#include "Mover.h"
+
+class Rocket : public Mover
+{
+public:
+    Rocket();
+    Rocket(double mph);
+
+    string move() override;
+};
+
+#endif
